8e5ece2c4da6dcb247998597be2cfd9867acf54e
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-05-19 23:57:22 +0200

    added notes

---
+++ dockerbaxter/README.md
@@ -0,0 +1,14 @@
+
+retetaum / percro2015
+baxter  192.168.0.2
+
+192.168.0.2     011411P0013.local
+
+baxter.sh 
+fix https://bitbucket.org/edistefano/taum
+
+missing nameserver: /etc/resolv.conf nameser
+
+manually clone this:  https://github.com/ros-drivers/openni2_launch
+
+
---
+++ dockerbaxter/runros.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#    --cpuset-cpus 0 \ # control the cpu
+#    --memory 512mb \ # max memory it can use
+#    -v $HOME/Downloads:/root/Downloads \ # optional, but nice
+#    -v $HOME/.config/google-chrome/:/data \ # if you want to save state
+#
+#
+# REQUIRES xhost+
+# See: http://wiki.ros.org/docker/Tutorials/GUI
+TARGET=osrf/ros:indigo-desktop-full-nvidia
+TARGET=u1404rosindigoxfce
+TARGET=baxteru1404
+export MYNVIDIA=$(curl -s http://localhost:3476/v1.0/docker/cli?dev=0\&vol=nvidia_driver)
+export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
+#--device /dev/nvidia0 --device /dev/nvidiactl --device /dev/bus/usb:/dev/bus/usb:rwm"
+sudo docker run -it --net host -e QT_X11_NO_MITSHM=1 $MYNVIDIA -v /usr/bin/X11/xeyes:/usr/bin/X11/xeyes -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/snd $TARGET
+#TODO fix 
+#TODO add 
+#source /opt/ros/indigo/setup.bash
+#rocore &
+
