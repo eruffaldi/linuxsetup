fec058a02667cac62bffbca880437d5775c17368
Lorenzo Peppoloni <l.peppoloni@gmail.com>
2016-05-20 17:00:34 +0200

    small update

--- dockerbaxter/runros.sh
+++ dockerbaxter/runros.sh
@@ -10,10 +10,13 @@
 TARGET=osrf/ros:indigo-desktop-full-nvidia
 TARGET=u1404rosindigoxfce
 TARGET=baxteru1404
-export MYNVIDIA=$(curl -s http://localhost:3476/v1.0/docker/cli?dev=0\&vol=nvidia_driver)
+#export MYNVIDIA=$(curl -s http://localhost:3476/v1.0/docker/cli?dev=0\&vol=nvidia_driver)
+#export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
 export MYNVIDIA="-e LD_LIBRARY_PATH=/usr/local/nvidia/lib64 --volume=nvidia_driver_361.42:/usr/local/nvidia:ro --device=/dev/nvidia0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm"
+xhost + > /dev/zero
 #--device /dev/nvidia0 --device /dev/nvidiactl --device /dev/bus/usb:/dev/bus/usb:rwm"
-sudo docker run -it --net host -e QT_X11_NO_MITSHM=1 $MYNVIDIA -v /usr/bin/X11/xeyes:/usr/bin/X11/xeyes -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/snd $TARGET
+# -v /usr/bin/X11/xeyes:/usr/bin/X11/xeyes
+sudo docker run -it --net host -e QT_X11_NO_MITSHM=1 $MYNVIDIA -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --device /dev/snd $TARGET
 #TODO fix 
 #TODO add 
 #source /opt/ros/indigo/setup.bash
