1c9996b959393bbf0f7aba3492f7bb7471da69b4
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-10-21 12:06:12 +0200

    added variable check

--- linuxbase.sh
+++ linuxbase.sh
@@ -2,12 +2,23 @@
 #
 #UBUNTU NAMES
 #precise 12.04
-#xenial  14.04
+#trusty  14.04
+#xenial  16.04
 #
 #ROS NAMES
 #hydro   
 #indigo LTS
 #jade
+#kinetic LTS
+
+if [ -z "$UBUNTUNAME" ]; then
+    echo "Need to set UBUNTUNAME: precise trusty xenial"
+    exit 1
+fi  
+if [ -z "$ROSNAME" ]; then
+    echo "Need to set ROSNAME: hydro indigo jade kinetic"
+    exit 1
+fi  
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $UBUNTUNAME main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-get update
 apt-get -q -y --force-yes  install ros-$ROSNAME-desktop-full
